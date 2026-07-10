e4f8223443301e7e9907283ba10b94fdb55d11c4
Suraj Kumar <ydvsurajbgp@gmail.com>
2026-07-10 19:53:48 +0530

    Update largest_element.cpp

--- largest_element.cpp
+++ largest_element.cpp
@@ -1,37 +1,35 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>   // For sort()
 using namespace std;
 
+// BRUTE FORCE METHOD
 class Solution {
 public:
-    int largestElement(vector<int> &arr) {
-        int largest = arr[0];
+    int largestElement(vector<int>& nums) {
 
-        for (int i = 0; i < arr.size(); i++) {
-            if (arr[i] > largest) {
-                largest = arr[i];
-            }
-        }
+        sort(nums.begin(), nums.end());
 
-        return largest;
+        return nums[nums.size() - 1];
     }
 };
 
 int main() {
+
     Solution obj;
 
     int n;
     cout << "Enter the size of the array: ";
     cin >> n;
 
-    vector<int> arr(n);
+    vector<int> nums(n);
 
     cout << "Enter the elements: ";
     for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+        cin >> nums[i];
     }
 
-    int ans = obj.largestElement(arr);
+    int ans = obj.largestElement(nums);
 
     cout << "Largest element = " << ans << endl;
 
