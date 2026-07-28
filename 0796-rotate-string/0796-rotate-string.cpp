b829b37c94955b8891098a87f8ff7fc2725ef79d
SURAJ KUMAR <142513131+Suraj76450@users.noreply.github.com>
2026-07-28 23:00:29 +0530

    Time: 0 ms (100%), Space: 8 MB (69.65%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+
+        // Length must be same
+        if (s.length() != goal.length()){
+            return false;}
+
+        // Join string with itself
+        string combined = s + s;
+
+        // Check if goal is present inside combined
+        if (combined.find(goal) != string::npos)
+            return true;
+
+        return false;
+    }
+};
