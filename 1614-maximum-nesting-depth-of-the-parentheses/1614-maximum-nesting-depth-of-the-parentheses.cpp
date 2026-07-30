f858d4c00b12d22b05a2f5c4917eb285e41d7f65
SURAJ KUMAR <142513131+Suraj76450@users.noreply.github.com>
2026-07-30 20:22:21 +0530

    Time: 0 ms (100%), Space: 8.4 MB (24.69%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,37 +1,55 @@
-// Optimal Approach
+// // Optimal Approach
+
+// class Solution {
+// public:
+//     int maxDepth(string s) {
+
+//         // Store the current depth of parentheses
+//         int depth = 0;
+
+//         // Store the maximum depth found
+//         int maxDepth = 0;
+
+//         // Traverse the string
+//         for (int i = 0; i < s.size(); i++) {
+
+//             // If an opening parenthesis is found
+//             if (s[i] == '(') {
+
+//                 // Increase the current depth
+//                 depth++;
+
+//                 // Update the maximum depth
+//                 maxDepth = max(maxDepth, depth);
+//             }
+
+//             // If a closing parenthesis is found
+//             else if (s[i] == ')') {
+
+//                 // Decrease the current depth
+//                 depth--;
+//             }
+//         }
+
+//         // Return the maximum depth of parentheses
+//         return maxDepth;
+//     }
+// };
 
 class Solution {
 public:
     int maxDepth(string s) {
-
-        // Store the current depth of parentheses
-        int depth = 0;
-
-        // Store the maximum depth found
-        int maxDepth = 0;
-
-        // Traverse the string
-        for (int i = 0; i < s.size(); i++) {
-
-            // If an opening parenthesis is found
+        int n = s.length();
+        int count = 0;
+        int maxCount = 0;
+        for (int i = 0; i < n; i++) {
             if (s[i] == '(') {
-
-                // Increase the current depth
-                depth++;
-
-                // Update the maximum depth
-                maxDepth = max(maxDepth, depth);
-            }
-
-            // If a closing parenthesis is found
-            else if (s[i] == ')') {
-
-                // Decrease the current depth
-                depth--;
+                count++;
+                maxCount = max(count, maxCount);
+            }else if(s[i] == ')'){
+                count--;
             }
         }
-
-        // Return the maximum depth of parentheses
-        return maxDepth;
+        return maxCount;
     }
 };
