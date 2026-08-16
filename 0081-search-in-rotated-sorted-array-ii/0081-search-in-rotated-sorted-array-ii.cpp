baeafe275a87b18ba9aca76a0c8ff6e4f11e8940
SURAJ KUMAR <142513131+Suraj76450@users.noreply.github.com>
2026-08-16 19:55:38 +0530

    Time: 0 ms (100%), Space: 18 MB (81.83%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,13 +1,54 @@
+// Optimal Approach: Modified Binary Search
+// Time: O(log n) average, O(n) worst case due to duplicates
+// Space: O(1)
+
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        int n = nums.size();
-        for(int i = 0; i< n; i++){
-            if(nums[i] == target){
+
+        int low = 0;
+        int high = nums.size() - 1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            // Target found
+            if (nums[mid] == target) {
                 return true;
             }
+
+            // Special case: duplicates
+            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+                low++;
+                high--;
+            }
+
+            // Left half is sorted
+            else if (nums[low] <= nums[mid]) {
+
+                // Target lies in left sorted half
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                }
+                else {
+                    low = mid + 1;
+                }
+            }
+
+            // Right half is sorted
+            else {
+
+                // Target lies in right sorted half
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                }
+                else {
+                    high = mid - 1;
+                }
+            }
         }
+
         return false;
-        
     }
 };
