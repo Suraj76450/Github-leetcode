487ba3b9b194d6d062bdd208a4a9b29317eb3c78
SURAJ KUMAR <142513131+Suraj76450@users.noreply.github.com>
2026-09-22 18:19:50 +0530

    Time: 0 ms (100%), Space: 12.6 MB (54.09%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,18 +1,52 @@
+// Linear Search Approach
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
 
         int n = nums.size();
 
+        // Only one element
+        if (n == 1)
+            return 0;
+
         for (int i = 0; i < n; i++) {
 
-            if ((i == 0 || nums[i] > nums[i - 1]) &&
-                (i == n - 1 || nums[i] > nums[i + 1])) {
-
+            // First element
+            if (i == 0 && nums[i] > nums[i + 1])
                 return i;
-            }
+
+            // Last element
+            if (i == n - 1 && nums[i] > nums[i - 1])
+                return i;
+
+            // Middle element
+            if (i > 0 && i < n - 1 && nums[i] > nums[i - 1] &&
+                nums[i] > nums[i + 1])
+                return i;
         }
 
         return -1;
     }
 };
+
+// class Solution {
+// public:
+//     int findPeakElement(vector<int>& nums) {
+
+//         int n = nums.size();
+
+//         for (int i = 0; i < n; i++) {
+
+//             if ((i == 0 || nums[i] > nums[i - 1]) &&
+//                 (i == n - 1 || nums[i] > nums[i + 1])) {
+
+//                 return i;
+//             }
+//         }
+
+//         return -1;
+//     }
+// };
