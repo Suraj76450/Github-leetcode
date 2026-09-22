3a99c13ea9dbcafde6260d8a60272aa9371f69f4
SURAJ KUMAR <142513131+Suraj76450@users.noreply.github.com>
2026-09-23 00:24:08 +0530

    Time: 0 ms (100%), Space: 12.7 MB (17.15%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -23,8 +23,7 @@ public:
                 return i;
 
             // Middle element
-            if (i > 0 && i < n - 1 && nums[i] > nums[i - 1] &&
-                nums[i] > nums[i + 1])
+            if (i > 0 && i < n - 1 && nums[i] > nums[i - 1] && nums[i] > nums[i + 1])
                 return i;
         }
 
